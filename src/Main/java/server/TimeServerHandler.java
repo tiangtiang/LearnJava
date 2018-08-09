52c0ea7a41d18ae2df4ed1e23172741608859c20
tiang437 <tiangloveannie@163.com>
2018-08-09 10:48:28 +0800

    netty框架demo

---
+++ src/Main/java/server/TimeServerHandler.java
@@ -0,0 +1,25 @@
+package server;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelFutureListener;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+
+/**
+ * Created by tiang on 2018/8/8.
+ */
+public class TimeServerHandler extends ChannelInboundHandlerAdapter {
+    @Override
+    public void channelActive(final ChannelHandlerContext ctx) throws Exception {
+        final ByteBuf time = ctx.alloc().buffer(4); // (2)
+        time.writeInt((int) (System.currentTimeMillis() / 1000L + 2208988800L));
+        final ChannelFuture f = ctx.writeAndFlush(time); // (3)
+        f.addListener(ChannelFutureListener.CLOSE); // (4)
+    }
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) {
+        cause.printStackTrace();
+        ctx.close();
+    }
+}
